28323485a92b039a112bf7edda73e349f03ca860
DhaniswarBK <bkdhanishowor@gmail.com>
2023-03-13 13:51:29 +0545

    Practice

---
+++ Concept/index.js
@@ -0,0 +1,11 @@
+const d  = new Date();
+console.log(d)
+
+const d1  = new Date(100000000000);
+console.log(d1)
+
+const d3 = new Date("2015-03-25");
+console.log(d3)
+
+const d4 = new Date(2018, 11, 24, 10, 33, 30, 0);
+console.log(d4)
